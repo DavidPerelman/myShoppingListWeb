69b97a99a0201f1488ed43165b295771938661c9
David Perelman <dperelman3@gmail.com>
2020-03-11 21:46:55 +0200

    post article with axios

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component} from "react";
 import Articles from './Articles';
+import PostArticle from './PostArticle';
 
 class App extends Component{
 
@@ -7,6 +8,7 @@ class App extends Component{
     return(
         <div className="App">
         <Articles />
+        <PostArticle />
       </div>
     );
   }
---
+++ client/src/components/PostArticle.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+class PostArticle extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: '',
+            description: '',
+            content: ''
+        }
+    }
+
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state);
+        axios.post('http://localhost:5000/articles', this.state)
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+    render() {
+        const { title, description, content } = this.state
+        return (
+                <div>
+                    <form onSubmit={this.submitHandler}>
+                        <div>
+                            <input type="text" name="title" value={title} onChange={this.changeHandler} />
+                        </div>
+                        <div>
+                            <input type="text" name="description" value={description}onChange={this.changeHandler} />
+                        </div>
+                        <div>
+                            <input type="text" name="content" value={content}onChange={this.changeHandler} />
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+        )
+    }
+}
+
+export default PostArticle;
